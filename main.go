9921dd382c822a1f3af46fed0a11e3ea15328267
Higashiii <37910197+Higashi-J@users.noreply.github.com>
2019-01-11 14:06:47 +0900

    main.go updated

--- main.go
+++ main.go
@@ -1,9 +1,10 @@
 package main
 
-import(
+import (
 	"html/template"
 	"net/http"
 )
+
 //盤面で使う文字列
 const maru, batsu = "〇", "×"
 
@@ -11,18 +12,32 @@ const maru, batsu = "〇", "×"
 type Board [3][3]string
 
 type ViewData struct {
-	Turn string
+	Turn  string
 	Board *Board
 }
+
 //templateの設定
 var tmpl *template.Template = template.Must(template.ParseFiles("game.tmpl"))
 
 func (v *ViewData) Execute(w http.ResponseWriter) {
 	w.Header().Set("Content-Type", "text/html; charset=utf-8")
-	if err := tmpl.Execute(w,v); err != nil {
+	if err := tmpl.Execute(w, v); err != nil {
 		panic(err)
 	}
 }
+
+//gameHandle関数の宣言
 func gameHandle(w http.ResponseWriter, r *http.Request) {
-	
+	turn := maru
+	board := &Board{}
+	v := ViewData{turn, board}
+	v.Execute(w)
+}
+
+func main() {
+	http.HandleFunc("/game", gameHandle)
+
+	if err := http.ListenAndServe(":4000", nil); err != nil {
+		panic(err)
+	}
 }
